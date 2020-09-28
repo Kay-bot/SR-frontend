bfec08eb1e23d3839bf1538cf4752f09318632aa
KAY S <KS.KWANRUAN@GMAIL.com>
2020-09-29 08:43:32 +1000

    staff

---
+++ components/InnerHeader.js
@@ -0,0 +1,11 @@
+const InnerHeader = () => {
+  return (
+    <header>
+      <div className="px-4 py-4 bg-primary text-white">
+        <h1>Our Team</h1>
+      </div>
+    </header>
+  );
+};
+
+export default InnerHeader;
--- pages/our-team/index.tsx
+++ pages/our-team/index.tsx
@@ -1,9 +1,10 @@
 import Layout from '../../components/Layout';
 import { Container } from 'reactstrap';
-
+import InnerHeader from '../../components/InnerHeader';
 const OurTeam = () => {
   return (
     <Layout>
+      <InnerHeader />
       <Container>
         <div>Our Team</div>
       </Container>
