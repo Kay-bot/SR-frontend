8f4d49f84db996e100ef3a8b12d2961da5dbe294
KAY S <KS.KWANRUAN@GMAIL.com>
2020-09-30 23:30:37 +1000

    our team

--- components/OurTeamComponent.js
+++ components/OurTeamComponent.js
@@ -5,47 +5,48 @@ const OurTeamComponent = () => {
   return (
     <section>
       <div
-        className={classnames('text-center bg-light py-3', styles.meetTheTeam)}
+        className={classnames('text-center bg-light py-5', styles.meetTheTeam)}
       >
         <h2 className="section-title">
-          Meet the <span>team</span>
+          Meet The <span>Team</span>
         </h2>
         <div className="bottom-line"></div>
       </div>
-
-      <div>
+      <div className={classnames('container', styles['leadership'])}>
         <h2>Leadership</h2>
-      </div>
-      <div className={classnames(styles['leadership'])}>
         <div className={classnames('text-center py-4', styles.ourTeam)}>
           <img src="/staff/mark.jpg" />
           <h4>Mark A White</h4>
           <b>Managing Director.</b>
-          {/* <p>
-            Mark holds a 30 years of roofing experience, with over 15 years as a
-            strata specialist. He is a former member of the SCA (Strata Service
-            Committee) and is a SCA Qualified strata specialist. Mark takes much
-            pride in our company's excellent reputation for being honest with
-            high integrity and quality standard. He also takes equal pride in
-            our high level of customer service and our award-winning
-            professional team. Mark’s personal time is taken up in his
-            leadership role in local community service, and contributing for a
-            better Australia.{' '}
-          </p> */}
+          <div>
+            <p>
+              Mark holds a 30 years of roofing experience, with over 15 years as
+              a strata specialist. He is a former member of the SCA (Strata
+              Service Committee) and is a SCA Qualified strata specialist. Mark
+              takes much pride in our company's excellent reputation for being
+              honest with high integrity and quality standard. He also takes
+              equal pride in our high level of customer service and our
+              award-winning professional team. Mark’s personal time is taken up
+              in his leadership role in local community service, and
+              contributing for a better Australia.{' '}
+            </p>
+          </div>
         </div>
         <div className={classnames('text-center py-4', styles.ourTeam)}>
           <img src="/staff/Rob.jpg" />
           <h4>Robert Robinson</h4>
           <b>Operations Manager & Second in Charge</b>
-          {/* <p>
-            Rob is amongst the most knowledgeable roofing specialist (Tile +
-            Metal) in the industry. He has 45 years’ experience, and is also, a
-            SCA qualified strata specialist. Rob is a hard ‘task’ master with
-            our tradesman, his attention to detail is consistent. He is
-            responsible for the day to day management of the on-the-road team
-            and ensures every client is happy with the outcome of the works. Rob
-            has been with the company for 9 years.
-          </p> */}
+          <div>
+            <p>
+              Rob is amongst the most knowledgeable roofing specialist (Tile +
+              Metal) in the industry. He has 45 years’ experience, and is also,
+              a SCA qualified strata specialist. Rob is a hard ‘task’ master
+              with our tradesman, his attention to detail is consistent. He is
+              responsible for the day to day management of the on-the-road team
+              and ensures every client is happy with the outcome of the works.
+              Rob has been with the company for 9 years.
+            </p>
+          </div>
         </div>
       </div>
       <div>
