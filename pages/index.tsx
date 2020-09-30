4740262798bc4ec0fb8f1e8f6e41dcb8452d457a
KAY S <KS.KWANRUAN@GMAIL.com>
2020-09-30 18:10:27 +1000

    carousel remove

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,10 @@
 import Layout from '../components/Layout';
 import { Container } from 'reactstrap';
-import Carousel from '../components/Carousel';
 import HowCanWeHelp from '../components/HowCanWeHelp';
 import WhyUs from '../components/WhyUs';
 const Index = () => {
   return (
     <Layout>
-      <Carousel />
       <Container>
         <div>
           <HowCanWeHelp />
