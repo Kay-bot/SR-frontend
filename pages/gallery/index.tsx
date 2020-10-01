f371d53e453ac98c3b0b34799a02e2766781714b
KAY S <KS.KWANRUAN@GMAIL.com>
2020-10-01 12:56:22 +1000

    photo gallery

--- pages/gallery/index.tsx
+++ pages/gallery/index.tsx
@@ -1,11 +1,11 @@
 import Layout from '../../components/Layout';
 import { Container } from 'reactstrap';
-
+import PhotosGallery from '../../components/PhotosGallery';
 const Gallery = () => {
   return (
     <Layout>
       <Container>
-        <div>Coming Soon</div>
+        <PhotosGallery />
       </Container>
     </Layout>
   );
