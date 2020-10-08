723b5ad2e43a3f8eb134fc1fff446c30436e7e82
KAY S <KS.KWANRUAN@GMAIL.com>
2020-10-08 11:12:02 +1100

    bold heading and fixed typo

--- components/OurTeamComponent.js
+++ components/OurTeamComponent.js
@@ -22,11 +22,11 @@ const OurTeamComponent = () => {
             <b>Managing Director.</b>
             <div>
               <p>
-                Mark holds a 30 years of roofing experience, with over 15 years
-                as a strata specialist. He is a former member of the SCA (Strata
-                Service Committee) and is a SCA Qualified strata specialist.
-                Mark takes much pride in our company's excellent reputation in
-                the strata community for being honest with high integrity and
+                Mark has a 30 years of roofing experience, with over 15 years as
+                a Strata Specialist. He is a former member of the SCA (Strata
+                Service Committee) and is a SCA Qualified Strata Specialist.
+                Mark takes much pride in our company's excellent reputation, in
+                the strata community, for being honest with high integrity and
                 quality control. He also takes equal pride in our high level of
                 customer service and our award-winning professional team. Mark’s
                 personal time is taken up in his leadership role in local
@@ -42,12 +42,12 @@ const OurTeamComponent = () => {
               <p>
                 Rob is amongst the most knowledgeable roofing specialist (Tile &
                 Metal) in the industry. He has 45 years’ experience, and is
-                also, a SCA qualified strata specialist. Rob is a hard ‘task
+                also, a SCA Qualified Strata Specialist. Rob is a hard ‘task
                 master' with our tradesman, his attention to detail is
                 consistent. He is responsible for the day to day management of
                 the on-the-road team and ensures every client is happy with the
-                outcome of the works. Rob has been with the company for 9 years
-                and has a mature no nonesense attitude.
+                outcome of the works. Rob has been with the company for 10 years
+                who has a no nonesense attitude.
               </p>
             </div>
           </div>
@@ -56,7 +56,7 @@ const OurTeamComponent = () => {
 
       <div className={classnames(styles['staff'])}>
         <div>
-          <h2>Office Team</h2>
+          <h2>Office/Support Team</h2>
         </div>
         <hr></hr>
         <div></div>
@@ -70,7 +70,7 @@ const OurTeamComponent = () => {
                 Iram has a significant background in building and maintaining
                 client relationships. She works closely with the clients to
                 ensure the seamless coordination of their needs in professional
-                and timely manner. Her favourite subject being psychology, she
+                and timely manner. Her favourite subject, being psychology, she
                 strongly believes{' '}
                 <span>
                   "I have learnt that people will forget what you said or did
@@ -89,11 +89,12 @@ const OurTeamComponent = () => {
             <div>
               <p>
                 Ramya is a Qualified Construction Engineer, holding more than 6
-                years of experience and is the brains behind our thorough
+                years of actual experience and is the brains behind our thorough
                 training regime. Ramya ensures competitive pricing for our
-                quotes and which, ensures smooth operations with a strong eye
-                for detail, skilled problem solving and an experienced solution
-                specialist. She is also focused on product quality and source.
+                quotes, which ensures smooth operations, and she has a strong
+                eye for detail, skilled problem solving and an experienced
+                solution specialist. She is also focused on product quality and
+                available source.
               </p>
             </div>
           </div>
@@ -101,14 +102,14 @@ const OurTeamComponent = () => {
           <div className={classnames(styles.ourTeam)}>
             <img src="/staff/Ria.jpg" />
             <h4>Ria Isbell Lewis</h4>
-            <b>Accounts/Office Mamanger</b>
+            <b>Accounts/Office Manager</b>
             <div>
               <p>
-                Ria is a graduate with Master of professional Accounting from
+                Ria is a graduate with Master of Professional Accounting from
                 Australian Catholic University. Ria takes care of the accounts
                 and is responsible for reviewing and revising financials. As an
-                Office Manager she makes certain that the team reshapes itself
-                for the needs of the client and give the best outcome.
+                Office Manager, she makes certain that the team co-ordinates
+                from stage to stage, in progress.
               </p>
             </div>
           </div>
@@ -134,7 +135,7 @@ const OurTeamComponent = () => {
                 Jira is a graduate of Business Administration and IT. She
                 previously worked as a Key Accounts Manager. Jira takes immense
                 care in making sure the job is completed accurately to the quote
-                and takes care of the scheduled works carlendar to be completed.
+                and takes care of the scheduled works calendar to be completed.
               </p>
             </div>
           </div>
@@ -143,7 +144,7 @@ const OurTeamComponent = () => {
       {/* End */}
       <div className={classnames('bg-light', styles['staff'])}>
         <div>
-          <h2>Fieldwork / Onsite Team</h2>
+          <h2>Fieldwork/On-site Team</h2>
         </div>
         <hr></hr>
 
@@ -154,12 +155,12 @@ const OurTeamComponent = () => {
             <b>Licensed Supervisor & Senior Estimator</b>
             <div>
               <p>
-                10 years with Strata Roofing and over 40 years roofing
+                10 years with Strata Roofing and over 40 years of roofing
                 experience servicing all types of roofs. Ray is arguably the
                 best estimator in NSW – knowledge & skill are his best assets.
-                Ray is also skilled at producing ‘no-nonsense’ reports that non
-                trades people can easily understand that allows them to make
-                informed decisions.{' '}
+                Ray is also skilled at producing ‘no-nonsense’ reports that
+                non-trades people can easily understand, that allows them to
+                make informed decisions.
               </p>
             </div>
           </div>
@@ -170,11 +171,11 @@ const OurTeamComponent = () => {
             <b>Supervisor & Estimator</b>
             <div>
               <p>
-                Over 30 years roofing experience servicing all types of roofs.
-                Stuart is a driven performer with metal, tile roofs and also
-                guttering and rainwater systems. He has a keen interest in
-                ‘listening’ to the residents needs and requirements. Stuart is
-                also skilled at producing ‘no-nonsense’ reports that non trades
+                Over 30 years of roofing experience servicing all types of
+                roofs, Stuart is a driven performer with metal and tile roofs.
+                Also guttering and rainwater systems. He has a keen interest in
+                ‘listening’ to the resident's needs and requirements. Stuart is
+                also skilled at producing ‘no-nonsense’ reports that non-trades
                 people can easily understand that allows them to make informed
                 decisions.
               </p>
@@ -187,8 +188,8 @@ const OurTeamComponent = () => {
             <b>Membrane Roofing Division Manager</b>
             <div>
               <p>
-                30 years Membrane roof experience and a key member of our
-                management team. Kevin joined us 7 years ago and is well known,
+                30 years of Membrane roof experience and a key member of our
+                management team, Kevin joined us 8 years ago and is well known,
                 as one of the most respected experts of membrane roofs in
                 Sydney.
               </p>
@@ -200,10 +201,11 @@ const OurTeamComponent = () => {
             <b>Gutter Maintenance Division Supervisor</b>
             <div>
               <p>
-                Best we have ever had running this difficult service! Thomas
-                joined us 2 years ago and has proven himself to be a reliable,
-                honest likeable man. Our resident “Frenchman”, is also gradually
-                putting together our future anchor point installation division.
+                The best we have ever had, running this difficult service!
+                Thomas joined us 2 years ago and has proven himself to be a
+                reliable, honest and likeable man. Our resident “Frenchman”, is
+                also gradually putting together our future anchor point
+                installation division.
               </p>
             </div>
           </div>
@@ -217,8 +219,8 @@ const OurTeamComponent = () => {
             <div>
               <p>
                 Training in all aspects of the business, Alex has been with the
-                company for 5 years. Alex, has a hard task master with his
-                father, (Mark) however, has successfully trained already with
+                company for 6 years. Alex has a 'hard taskmaster' with his
+                father, Mark. However, he has successfully trained with
                 accounting, customer service and basic quote preparation.
               </p>
             </div>
