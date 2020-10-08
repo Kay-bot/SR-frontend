61d25c1376e6d2f18014fd863f55f0f1a2426d8c
KAY S <KS.KWANRUAN@GMAIL.com>
2020-10-08 11:18:17 +1100

    fixed content

--- components/WhyUs.js
+++ components/WhyUs.js
@@ -11,41 +11,23 @@ const OurFeatures = () => {
 
           <div className={classnames(styles['features'])}>
             <div>
-              {/* <i
-                className={classnames(
-                  'far fa-building fa-2x',
-                  styles['icon-color']
-                )}
-              ></i> */}
               <h3>How are we – Strata Specialist?</h3>
               <p>
                 We are the only Sydney roofing company, that exclusively
                 services the strata community sector. All of our office support
                 team and trades / supervisors are fully trained in strata
                 managed property’s needs and compliance laws and regulations.{' '}
+              </p>
+              <p>
                 <b>
                   We have been members of Strata Community Australia for many
                   years now & our senior management team have completed the SCA
                   Strata Specialist course.
                 </b>
-                <span>
-                  <a href="/our-services"> See more services...</a>
-                </span>
-              </p>
-              <p>
-                Call
-                <a href="tel:0294363006"> 02 9436 3006 </a>
-                now to discuss your next roof replacement project!
               </p>
             </div>
 
             <div>
-              {/* <i
-                className={classnames(
-                  'far fa-building fa-2x',
-                  styles['icon-color']
-                )}
-              ></i> */}
               <h3>Best Roof Reports – in Sydney!</h3>
               <p>
                 We are told so often, by so many strata managers & strata
@@ -57,27 +39,13 @@ const OurFeatures = () => {
                 <b>
                   These reports are free to our regular strata manager clients.
                 </b>
-                <span>
-                  <a href="/our-services"> See more services...</a>
-                </span>
-              </p>
-              <p>
-                Call
-                <a href="tel:0294363006"> 02 9436 3006 </a>
-                now for the perfect roof!
               </p>
             </div>
 
             <div>
-              {/* <i
-                className={classnames(
-                  'far fa-clock fa-2x',
-                  styles['icon-color']
-                )}
-              ></i> */}
               <h3>We’ve got you covered!</h3>
               <p>
-                We are 100% compliant, strata owners and managers can be totally
+                We are 100% compliant. Strata owners and managers can be totally
                 confident that we have all the insurances in place, being:{' '}
                 <b>
                   Public Liability / Workers Compensation &amp; Home Warranty
@@ -85,7 +53,7 @@ const OurFeatures = () => {
                 </b>{' '}
                 (HBCF). All insurance policies are independently audited each
                 year to give our clients 100% confidence that we are 100%
-                covered, thus all of our clients fully covered.
+                covered, thus all of our clients fully covered!
               </p>
               <p>
                 <b>
@@ -98,25 +66,14 @@ const OurFeatures = () => {
                 on-site residents, the general public &amp; our tradesmen is
                 equally important and is imbedded in our DNA.
               </p>
-              <p>
-                Call
-                <a href="tel:0294363006"> 02 9436 3006 </a>
-                us now. We’ll get it set up!
-              </p>
             </div>
             <div>
-              {/* <i
-                className={classnames(
-                  'far fa-thumbs-up fa-2x',
-                  styles['icon-color']
-                )}
-              ></i> */}
               <h3>Not all roofers are fully licensed!</h3>
               <p>
                 It’s a common mistake that some strata managers and many owners’
                 corporations ‘think’ that a licensed roofing contractor is
                 allowed to carry out ‘ALL’ roofing, this is simply wrong and can
-                in fact lead to{' '}
+                in fact can lead to{' '}
                 <u>
                   roofing works, being carried out by UNLICENSED contractors,
                 </u>
@@ -147,11 +104,6 @@ const OurFeatures = () => {
               <p>
                 <i className="fas fa-check"></i>
                 <span> Waterproofing</span> (Membrane Roofs)
-              </p>
-
-              <p>
-                Need a new roof or fast gutter replacement services? Call{' '}
-                <a href="tel:0294363006"> 02 9436 3006 </a> today.
               </p>
             </div>
           </div>
